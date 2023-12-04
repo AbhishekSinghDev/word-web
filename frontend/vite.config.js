2d0e10a67dd3bceb3204bad3d1cdf3fe9bcdc4d9
AbhishekSingh <siyanisabhishek@gmail.com>
2023-12-05 00:09:53 +0530

    fixed vite config for backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // server: {
+  //   proxy: {
+  //     "/api/v1": {
+  //       target: "https://long-puce-lamb-ring.cyclic.app/",
+  //       secure: false,
+  //     },
+  //   },
+  // },
   server: {
     proxy: {
-      "/api": {
-        target: "https://long-puce-lamb-ring.cyclic.app/",
-        secure: false,
-      },
+      "/api": "https://long-puce-lamb-ring.cyclic.app/",
     },
   },
   plugins: [react()],
