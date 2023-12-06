bec12b22ee8a1f1600f16356c0304a671549a107
AbhishekSingh <siyanisabhishek@gmail.com>
2023-12-06 17:50:18 +0530

    Trying to fix previous commit

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -5,8 +5,18 @@ import BlogCard from "./BlogCard";
 import axiosInstance from "../axiosInstance.js";
 import { Navigate } from "react-router-dom";
 
-import avatar1 from "../assets/images/avatars/1.png";
 import toast, { Toaster } from "react-hot-toast";
+
+import avatar1 from "../assets/images/avatars/1.png";
+import avatar2 from "../assets/images/avatars/2.png";
+import avatar3 from "../assets/images/avatars/3.png";
+import avatar4 from "../assets/images/avatars/4.png";
+import avatar5 from "../assets/images/avatars/5.png";
+import avatar6 from "../assets/images/avatars/6.png";
+import avatar7 from "../assets/images/avatars/7.png";
+import avatar8 from "../assets/images/avatars/8.png";
+import avatar9 from "../assets/images/avatars/9.png";
+import avatar10 from "../assets/images/avatars/10.png";
 
 const Profile = () => {
   const { user } = useContext(UserContext);
@@ -14,8 +24,20 @@ const Profile = () => {
   const [userBlogs, setUserBlogs] = useState([]);
   const [userAvatar, setUserAvatar] = useState(avatar1);
   useEffect(() => {
+    const profileIconArrays = [
+      avatar1,
+      avatar2,
+      avatar3,
+      avatar4,
+      avatar5,
+      avatar6,
+      avatar7,
+      avatar8,
+      avatar9,
+      avatar10,
+    ];
     const randomAvatar = Math.floor(Math.random() * 11);
-    setUserAvatar(`../assets/images/avatars/${randomAvatar}.png`);
+    setUserAvatar(profileIconArrays[randomAvatar]);
   }, [userAvatar]);
 
   useEffect(() => {
