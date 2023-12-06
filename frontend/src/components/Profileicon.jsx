f962ed1a4faf78525d6c3b027df305fa3e3c840c
AbhishekSingh <siyanisabhishek@gmail.com>
2023-12-06 17:55:36 +0530

    Fixing the same bug, in profileicon component

--- frontend/src/components/Profileicon.jsx
+++ frontend/src/components/Profileicon.jsx
@@ -1,4 +1,13 @@
 import avatar1 from "../assets/images/avatars/1.png";
+import avatar2 from "../assets/images/avatars/2.png";
+import avatar3 from "../assets/images/avatars/3.png";
+import avatar4 from "../assets/images/avatars/4.png";
+import avatar5 from "../assets/images/avatars/5.png";
+import avatar6 from "../assets/images/avatars/6.png";
+import avatar7 from "../assets/images/avatars/7.png";
+import avatar8 from "../assets/images/avatars/8.png";
+import avatar9 from "../assets/images/avatars/9.png";
+import avatar10 from "../assets/images/avatars/10.png";
 
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
@@ -6,9 +15,21 @@ import { Link } from "react-router-dom";
 const Profileicon = () => {
   const [userAvatar, setUserAvatar] = useState(avatar1);
   useEffect(() => {
+    const profileIconArrays = [
+      avatar1,
+      avatar2,
+      avatar3,
+      avatar4,
+      avatar5,
+      avatar6,
+      avatar7,
+      avatar8,
+      avatar9,
+      avatar10,
+    ];
     const randomAvatar = Math.floor(Math.random() * 11);
-    setUserAvatar(`../../src/assets/images/avatars/${randomAvatar}.png`);
-  }, []);
+    setUserAvatar(profileIconArrays[randomAvatar]);
+  }, [userAvatar]);
 
   const handleLogout = (e) => {
     e.preventDefault();
